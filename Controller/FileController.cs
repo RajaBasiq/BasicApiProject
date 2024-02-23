eb723074e18552e8c193edf223ce3aea2d28a7b5
Raja Basiq <bashraf@SSI-BASIQ-LP>
2024-02-23 06:21:37 +0500

    Add project files.

---
+++ Controller/FileController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasicApiProject.Controller
+{
+    [ApiController]
+    [Route("file")]
+    public class FileController : ControllerBase
+    {
+        [HttpGet]
+        [Route("GetFile")]
+        public IActionResult GetFile(int fileId) 
+        {
+            try
+            {
+                if(fileId > 0) 
+                {
+                    return Ok(fileId);
+                }
+                return BadRequest("file id must be greater then 0");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
