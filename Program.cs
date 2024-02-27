0b4a2ac67705aa6fdd4a6d5b6920f13095c95c46
Raja Basiq <bashraf@SSI-BASIQ-LP>
2024-02-28 00:01:51 +0500

    Enable Cors

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace BasicApiProject
             builder.Services.AddControllers();
             
             var app = builder.Build();
-
+            app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.MapControllers();
             app.Run();
         }
