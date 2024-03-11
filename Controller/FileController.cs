4421ca968325006e0c9827a4616ba527d6b4b822
Raja Basiq <bashraf@SSI-BASIQ-LP>
2024-03-11 17:35:09 +0500

    Add class library Add File Metadata Dto Add File  Content Dto Add GetAll API End point

--- Controller/FileController.cs
+++ Controller/FileController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Dtos;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BasicApiProject.Controller
@@ -9,11 +10,11 @@ namespace BasicApiProject.Controller
     {
         [HttpGet]
         [Route("GetFile")]
-        public IActionResult GetFile(int fileId) 
+        public IActionResult GetFile(int fileId)
         {
             try
             {
-                if(fileId > 0) 
+                if (fileId > 0)
                 {
                     return Ok(fileId);
                 }
@@ -24,5 +25,32 @@ namespace BasicApiProject.Controller
                 return BadRequest(ex.Message);
             }
         }
+        [Route("GetAllFiles"), HttpGet]
+        public ActionResult<FileContent> GetAll() 
+        {
+            try 
+            {
+                var file=new FileContent();
+                file.ID = 5;
+                file.UserName = "admin";
+                file.Name = "BasicFile";
+                file.CreatedOn= DateTime.Now.AddHours(-5);
+                file.DateOfBirth = DateTime.Now;
+                if (file!=null)
+                {
+                    return Ok(file);
+                }
+                else 
+                {
+                    return BadRequest("File not found");
+                }
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+                return BadRequest(ex.Message);
+            }
+        }
+
     }
 }
